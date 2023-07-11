ef36b01b7096da617c773b4e2bd42047b9471a4e
Nanciico <FullZSY@outlook.com>
2023-07-11 15:31:17 +0800

    QuickSort

--- src/exercise2/Exercise2_4.java
+++ src/exercise2/Exercise2_4.java
@@ -1,2 +1,15 @@
-package exercise2;public class Exercise2_4 {
+package exercise2;
+
+public class Exercise2_4 {
+
+    public static void main(String[] args) {
+        Exercise2_4 runner = new Exercise2_4();
+
+
+    }
+}
+
+class SinkOptimizePQ {
+
+
 }
